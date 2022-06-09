9ea5a22418b349fefb361273b590e4f3e0208e52
Artem Soldatkin <russmenchik@gmail.com>
2022-06-09 05:44:33 -0500

    Test

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,10 +2,17 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 for param in "$@"; do
     echo "\$@ Parameter #$count = $param"
     6039742 (merge: @ instead *)
     4f402d6 (merge: @ instead *)
+=======
+
+while [[ -n "$1" ]]; do
+    echo "Parameter #$count = $1"
+    3ff5e85 (merge: use shift)
+>>>>>>> 9c4858a (Test)
     count=$(( $count + 1 ))
     shift
 done
